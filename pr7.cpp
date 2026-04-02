bd7f2916d16cf2752c2bf4361e9c19e5805d7eb2
ghosterenko <arsenijkonkov243@gmail.com>
2026-04-02 11:29:15 +0500

    Update pr7.cpp

--- pr7.cpp
+++ pr7.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
+struct Node {
+    char value;
+    Node* next;
+
+    Node(char Value) : value(Value), next(nullptr) {}
+};
 class Snake {
 public:
-    struct Node {
-        char value;
-        Node* next;
-    
-        Node(char Value) : value(Value), next(nullptr) {}
-    };
     Node* head = nullptr;
+    int size = 0;
     
     void add(char value) {
         Node* newN = new Node(value);
@@ -16,9 +17,10 @@ public:
             head = newN;
 
         head->next = newN;
+        size++;
     }
 
-    char display() {
+    void display() {
         Node* node = head;
         while (node != nullptr)
         {
@@ -26,40 +28,49 @@ public:
             node = node->next;
         }
     }
+    Node* node() {
+        if (head != nullptr)
+            return head;
+    }
 
 };
 
 class Field {
+public:
+    Snake snake;
     int width = 20;
     int height = 10;
-    char field[255];
+    char field[255][255];
 
-public:
+
     void draw() {
         srand(time(NULL));
-
+        Node* node = snake.node();
+        snake.add('0');
+        snake.add('0');
+        snake.add('0');
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                field[j] = '-';
+                field[i][j] = '-';
             }
-            field[i] = '\n';
+            field[i][0] = '\n';
+        }
+        for (int i = 0; i < snake.size; i++)
+        {
+            field[height / 2][width / 2] = node->value;
+            if (node->next != nullptr)
+                node = node->next;
         }
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                std::cout << field[j];
+                std::cout << field[i][j];
             }
         }
-    }
-    void drawSnake(Snake snake) {
-        snake.add('0');
-        snake.add('0');
-        snake.add('0');
-
-        snake.display();
+        
     }
     
 };
@@ -68,6 +79,7 @@ int main()
 {
     Snake snake = Snake();
     Field f = Field();
+    f.snake = snake;
     f.draw();
-    f.drawSnake(snake);
+
 }
